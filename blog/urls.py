0008a6110731cb0210564bf092ce5d643593eac8
Ben Williams <codebencode@icloud.com>
2025-01-20 11:50:34 +0000

    Add static css file and content to html pages

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home.html'),
-    path('blog/', views.PostList.as_view(), name='blog.html')
+    path('blog/', views.PostList.as_view(), name='blog.html'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
