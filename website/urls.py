252e1d45a0ca94d053436830019da0d4af439852
Ben Williams <codebencode@icloud.com>
2025-01-22 18:30:37 +0000

    Add summernote to style blog content

--- website/urls.py
+++ website/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path('', include('blog.urls'), name='blog-urls'),
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
