546761fa8175667a192936e24350aa757ff25390
Ben Williams <codebencode@icloud.com>
2025-01-22 12:02:40 +0000

    Add form to add a blog page

--- blog/models.py
+++ blog/models.py
@@ -16,11 +16,14 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
 
-class Meta:
-    ordering = ["-created_on"]
+    class Meta:
+        ordering = ["-created_on"]
 
-def __str__(self):
-    return f"{self.title} | written by {self.author}"
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=(str(self.id)))
 
 
 
